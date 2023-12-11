c53538e61b1cfd57d863ee26f06f53e1d995fd8b
Miicrop <103122613+Miicrop@users.noreply.github.com>
2023-12-11 21:35:37 +0100

    customTalker

--- ros1ws/src/custom_msg_ros1/scripts/talker.py
+++ ros1ws/src/custom_msg_ros1/scripts/talker.py
@@ -1,20 +1,35 @@
-#!/usr/bin/env python
-# license removed for brevity
-import rospy
-from custom_msg_ros1.msg import CustomMessage
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
-def talker():
-    pub = rospy.Publisher('chatter', CustomMessage, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        msg = CustomMessage()
-        msg.custom_value = 1.22
-        pub.publish(msg)
-        rate.sleep()
+from brics_actuator.msg import Poison 
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(Poison, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Poison()
+        msg.originator = "originator_ABC"
+        msg.description = "description_DEF"
+        msg.qos = 42.0
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Publishing: {msg.originator} {msg.description} {msg.qos}")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass 
+    main()
