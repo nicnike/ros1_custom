d26c703ee43aa26b1163c1b6269ae8ccbf99a69a
nicnike <57753615+nicnike@users.noreply.github.com>
2023-12-12 08:44:22 +0100

    Update talker.py

--- ros1ws/src/custom_msg_ros1/scripts/talker.py
+++ ros1ws/src/custom_msg_ros1/scripts/talker.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
+import rospy
+from brics_actuator.msg import Poison
 
-from brics_actuator.msg import Poison 
-
-class MinimalPublisher(Node):
-
+class PoisonPublisher(object):
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(Poison, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.publisher = rospy.Publisher('topic', Poison, queue_size=10)
+        rospy.init_node('poison_publisher', anonymous=True)
+        self.rate = rospy.Rate(10)  # 10hz
 
-    def timer_callback(self):
+    def publish_message(self):
         msg = Poison()
         msg.originator = "originator_ABC"
         msg.description = "description_DEF"
         msg.qos = 42.0
-        self.publisher_.publish(msg)
-        self.get_logger().info("Publishing: {} {} {}".format(msg.originator, msg.description, msg.qos))
 
-
-def main(args=None):
-    rclpy.init(args=args)
-
-    minimal_publisher = MinimalPublisher()
-    rclpy.spin(minimal_publisher)
-
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
-
+        while not rospy.is_shutdown():
+            self.publisher.publish(msg)
+            rospy.loginfo("Publishing: originator: %s, description: %s, qos: %f", msg.originator, msg.description, msg.qos)
+            self.rate.sleep()
 
 if __name__ == '__main__':
-    main()
+    try:
+        publisher = PoisonPublisher()
+        publisher.publish_message()
+    except rospy.ROSInterruptException:
+        pass
